52206af65ccb6f03bfcca797aca1994c6185e416
danif <danifaress@hotmail.com>
2020-03-16 19:01:46 +0100

    test1

--- app/src/main/java/com/example/utrack/MainActivity.kt
+++ app/src/main/java/com/example/utrack/MainActivity.kt
@@ -8,15 +8,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.decorView.apply {
+        //window.decorView.apply {
             // Hide both the navigation bar and the status bar.
             // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
             // a general rule, you should design your app to hide the status bar whenever you
             // hide the navigation bar.
-            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
-        }
+            //systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+        //}
 
-        setContentView(R.layout.settingspage)
+        setContentView(R.layout.usersettings)
     }
 }
 /*test*/
