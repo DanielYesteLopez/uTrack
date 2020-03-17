3233c924353b58942f2c6037c347cddd08bd1d83
jorgec4444 <vinagre444@gmail.com>
2020-03-17 13:08:39 +0100

    Link mainpage to settingpage

--- app/src/main/java/com/example/utrack/MainActivity.kt
+++ app/src/main/java/com/example/utrack/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.utrack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,7 +19,13 @@ class MainActivity : AppCompatActivity() {
             //systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
         //}
 
-        setContentView(R.layout.trainingpage)
+        setContentView(R.layout.mainpage)
+
+    }
+
+    fun onSettingsButtonPressed(view : View) {
+        val intent = Intent(this, SettingsActivity::class.java)
+        startActivity(intent)
     }
 }
 /*test*/
---
+++ app/src/main/java/com/example/utrack/SettingsActivity.kt
@@ -0,0 +1,15 @@
+package com.example.utrack
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settingspage)
+    }
+
+
+}
