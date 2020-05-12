645b9ae0e25f73a93f56915b7474508a50d4e716
jorgec4444 <vinagre444@gmail.com>
2020-05-12 20:18:02 +0200

    C006

---
+++ app/src/main/java/com/example/utrack/model/SessionList.kt
@@ -0,0 +1,76 @@
+package com.example.utrack.model
+
+import android.util.Log
+import java.io.FileWriter
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+
+class SessionList {
+    private var sessionList: ArrayList<Session>? = null
+
+    init {
+        sessionList = ArrayList()
+    }
+
+    fun addSession(session: Session) {
+        sessionList?.add(session);
+    }
+
+    fun deleteSession(index: Int) {
+        if (sessionList?.isEmpty()!!) {
+            Log.d("deleteSession", "The sessionList is empty")
+        } else if (index >= sessionList?.size!!) {
+            Log.d("deleteSession", "The index is incorrect")
+        } else {
+            sessionList?.removeAt(index)
+        }
+    }
+
+    fun deleteAll(){
+        sessionList?.clear()
+    }
+
+    fun getSession(index: Int) : Session? {
+        if (sessionList?.isEmpty()!!) {
+            Log.d("getSession", "The list is empty")
+            return null
+        }
+        else if (index >= sessionList!!.size){
+            Log.d("getSession", "Index is incorrect")
+            return null
+        }
+        return sessionList!![index]
+    }
+
+    fun getSessionList() : ArrayList<Session>? {
+        return sessionList
+    }
+
+    fun exportSession(path: String) {
+        val fileNameDateTimeFormat = SimpleDateFormat("yyyy_MMdd_HHmm")
+        val filePath = (path + "/"
+                + fileNameDateTimeFormat.format(Date()) + ".csv")
+
+        Log.d("exportSession()", "saving to $filePath")
+
+        var fileWriter: FileWriter? = null
+        try {
+            fileWriter = FileWriter(filePath, false)
+            for (session: Session in sessionList!!) {
+                fileWriter.append("${session.toString()} \n")
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            if (fileWriter != null) {
+                try {
+                    fileWriter.close()
+                } catch (ioe: IOException) {
+                    ioe.printStackTrace()
+                }
+            }
+        }
+    }
+}
